d1a0955093c35698be194c26849cda9162553e82
Oliaa <olga.paskova@gmail.com>
2022-07-27 16:22:47 +0300

    traffic lights graphic

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var pushButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      //redLightView.layer.cornerRadius = redLightView.bounds.height/2
+        //redLightView.clipsToBounds = true
+        
+        redLightView.layer.cornerRadius = 20
+        yellowLightView.layer.cornerRadius = 20
+        greenLightView.layer.cornerRadius = 20
+        
+        pushButton.layer.cornerRadius = 10
+        
     }
-
-
+    @IBAction func pushButtonPressed() {
+        
+    }
+    
 }
 
