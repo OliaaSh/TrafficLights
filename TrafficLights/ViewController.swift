1a84d83b6cf33aefc72411721939725da9e762d2
Oliaa <olga.paskova@gmail.com>
2022-07-30 17:00:43 +0300

    final change

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -16,21 +16,54 @@ class ViewController: UIViewController {
     
     @IBOutlet var pushButton: UIButton!
     
+   var lights = ""
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-      //redLightView.layer.cornerRadius = redLightView.bounds.height/2
-        //redLightView.clipsToBounds = true
         
+        lights = "All"
+        
+        redLightView.alpha = 1
+        yellowLightView.alpha = 1
+        greenLightView.alpha = 1
+        
+        super.viewDidLoad()
+     
         redLightView.layer.cornerRadius = 20
         yellowLightView.layer.cornerRadius = 20
         greenLightView.layer.cornerRadius = 20
-        
         pushButton.layer.cornerRadius = 10
         
     }
-    @IBAction func pushButtonPressed() {
-        
-    }
     
+    @IBAction func pushButtonPressed() {
+        if lights == "All" {
+            redLightView.alpha = 1
+            yellowLightView.alpha = 0.2
+            greenLightView.alpha = 0.2
+            
+            lights = "Red"
+            
+        } else  if lights == "Red" {
+            redLightView.alpha = 0.2
+            yellowLightView.alpha = 1
+            greenLightView.alpha = 0.2
+            
+            lights = "Yellow"
+            
+        } else if lights == "Yellow" {
+            redLightView.alpha = 0.2
+            yellowLightView.alpha = 0.2
+            greenLightView.alpha = 1
+            
+            lights = "All"
+        } 
+}
 }
 
+    
+   
+    
+    
+    
+    
+  
